124fed9b505453efe42df5509443515940d4d469
mizurova <mizurova@gmail.com>
2013-10-16 00:50:36 +0300

    Don't override protocol in $scope after saving

--- src/main/webapp/js/controller/ProtocolController.js
+++ src/main/webapp/js/controller/ProtocolController.js
@@ -1,14 +1,20 @@
 'use strict';
 
 indexApp.controller('ProtocolController',
-    function ProtocolController($scope, indexData, $routeParams, $timeout) {
+    function ProtocolController($scope, indexData, $routeParams, $timeout, $log) {
         $scope.protocol = indexData.getProtocol($routeParams.protocolId);
 
         var saveTimeout;
         $scope.saveProtocol = function (protocol) {
             $timeout.cancel(saveTimeout);
             saveTimeout = $timeout(function () {
-                $scope.protocol = indexData.saveProtocol(protocol);
+                indexData.saveProtocol(protocol)
+                    .then(function (protocol) {
+                        $log.info("Protocol saved");
+                    },
+                    function (response) {
+                        $log.error("Failed to save protocol");
+                    });
             }, 1000)
         };
     });
